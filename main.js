47689a844c40ffb7101504d3dbe391ac3f9c3484
quineshabaldwin13 <baldwinquinesha@gmailcom>
2021-10-21 12:36:08 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,11 @@
 const prompt = require("prompt-sync")({ sigint: true });
 // Chalk package to styling of the terminal
 const chalk = require('chalk');
+
+// Initialization
+console.log(chalk.red("========================================================================"));
+
+console.log(chalk.blue.bold("You've gone fishing! Try to maximize the value of your caught fish.",
+    "\n You can fish for six hours (till 12:00pm) and can catch at most 10 lbs of fish."));
+
+console.log(chalk.red("========================================================================"));
